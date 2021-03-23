801148501ee5de59ed51ea97a2df05f2c69fd858
Bit0r <nie_wang@outlook.com>
2021-03-23 21:21:39 +0800

    📦软件源配置

---
+++ config/registry.fish
@@ -0,0 +1,4 @@
+sudo pip config --global set global.index-url https://repo.huaweicloud.com/repository/pypi/simple
+sudo pip config --global set global.timeout 120
+sudo pip config --global set global.trusted-host repo.huaweicloud.com
+yarn config set registry https://repo.huaweicloud.com/repository/npm/
--- install.fish
+++ install.fish
@@ -4,7 +4,6 @@ function confirm
     [ $REPLY = y ]
 end
 
-
 # 安装基本配置
 if confirm 'Do you want to install configs?'
     source config/variables.fish
@@ -27,9 +26,13 @@ add-list 'Do you want to install C?' c
 add-list 'Do you want to install LNMP?' lnmp
 add-list 'Do you want to install nodejs?' nodejs
 add-list 'Do you want to install Python3 dependencies?' python3
-add-list 'Do you want to install some awesome cli software?' cli
-add-list 'Do you want to install some awesome gui software?' gui
-add-list 'Do you want to install some software documentation?' doc
+add-list 'Do you want to install Java?' java
+add-list 'Do you want to install Rust?' rust
+add-list 'Do you want to install some awesome cli softwares?' cli
+add-list 'Do you want to install some awesome gui softwares?' gui
+add-list 'Do you want to install nvidia-gui?' nvidia
+add-list 'Do you want to install some awesome gui softwares?' gui
+add-list 'Do you want to install some software docs?' doc
 add-list 'Do you want to install some awesome fonts?' fonts
 add-list 'Do you want to install office suite?' office
 
@@ -75,6 +78,11 @@ end
 if confirm 'Do you want to configure update-alternatives?'
     sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 40 --slave /usr/share/man/man1/python.1.gz python.1.gz /usr/share/man/man1/python3.1.gz
     sudo update-alternatives --install /usr/bin/editor editor /usr/bin/micro 80 --slave /usr/share/man/man1/editor.1.gz editor.1.gz /usr/share/man/man1/micro.1.gz
+end
+
+# 进行软件源配置
+if confirm 'Do you want to configure software registries?'
+    source config/registry.fish
 end
 
 # 将用户附加到组
