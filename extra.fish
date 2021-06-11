bdf0d33186dda89f90c9c3563f3efb78b93cf623
Bit0r <nie_wang@outlook.com>
2021-06-11 09:42:44 +0800

    📦 天翼校园启动器 🔧 增加一些软件包

--- extra.fish
+++ extra.fish
@@ -3,3 +3,9 @@
 if type -q pip
     pip install -r pkglist/requirements.txt
 end
+
+if ! type -q client
+    aria2c -d /tmp 'http://218.77.121.111/DownloadXml/ClientPkgs/64ac0526-0589-4ec9-9142-06db38ef3da2/HN-linux-clientV2-64.tar.gz'
+    sudo unar -o /opt /tmp/HN-linux-clientV2-64.tar.gz
+    sudo chmod -R 777 /opt/linux-clientV2.0-64/
+end
---
+++ functions/launch-surfing.fish
@@ -0,0 +1,5 @@
+function launch-surfing
+    /opt/linux-clientV2.0-64/
+    ./client
+    pkill ESurfingSvr
+end
